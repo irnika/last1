563b5dc0a06673cab1e36e2be6b9ddf5d5826ff2
Irnika <redduna@ukr.net>
2019-10-03 19:29:19 +0300

    11
    11

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//=../jquery/dist/jquery.js
+//=../popper/umd/popper.js
+//=..bootstrap/js/dist/tooltip.js
+//=..bootstrap/js/dist/popover.js
+//=..bootstrap/js/dist/collapse.js
+//=..bootstrap/js/dist/util.js
+
+//=custom/tooltip.js
+//=custom/popover.js
